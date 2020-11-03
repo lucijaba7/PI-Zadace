917c561dc20d01d63ba483426ec5c961bdbbdc61
unknown <babic.lucija1204@gmail.com>
2020-11-03 14:42:58 +0100

    Objekti i polja

--- JS-400/JS-405.js
+++ JS-400/JS-405.js
@@ -1,1 +1,49 @@
-aa
+let stand = {
+    naziv: "Adventski_stand",
+    lokacija: {
+        adresa: "Giardini 1",
+        postanski_broj: 52100,
+        grad: "Pula",
+        drzava: "Hrvatska"
+    },
+    radnici: [
+        {
+            ime: "Marko",
+            prezime: "Marić",
+            datum_rodenja: "16.05.1998."
+        },
+        {
+            ime: "Daria",
+            prezime: "Darić",
+            datum_rodenja: "01.08.1999."
+        }
+    ],
+    proizvodi: { 
+            hrana: [
+                {
+                    naziv: "Fritule",
+                    cijena: 10
+                },
+                {
+                    naziv: "Krofna",
+                    cijena: 8
+                },
+                {
+                    naziv: "Palacinke",
+                    cijena: 15
+                }
+            ],
+            pice: [
+                {
+                    naziv: "Topla cokolada",
+                    cijena: 15
+                },
+                {
+                    naziv: "Kuhano vino",
+                    cijena: 20 
+                }
+            ]
+        },
+    datumi_prodaje: ["20.12.2020.", "21.12.2020.", "22.12.2020.", "23.12.2020."]
+}
+
